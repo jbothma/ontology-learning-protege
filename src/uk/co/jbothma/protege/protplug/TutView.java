ee2b2a184fc48a320c65132f06e36cb215692079
JD Bothma <jbothma@gmail.com>
2012-07-04 19:10:53 +0200

    Initial commit
    tutorial plugin with one View component and gitignore.

---
+++ src/uk/co/jbothma/protege/protplug/TutView.java
@@ -0,0 +1,21 @@
+package uk.co.jbothma.protege.protplug;
+
+import org.protege.editor.owl.ui.view.AbstractOWLViewComponent;
+
+public class TutView extends AbstractOWLViewComponent {
+
+	private static final long serialVersionUID = 866185983362791061L;
+
+	@Override
+	protected void disposeOWLView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initialiseOWLView() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
